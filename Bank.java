9f7821bf30e61315705dda43a105cd60ce375944
adminsprobag <marknel.jayme21@gmail.com>
2026-01-18 22:16:44 +0800

    second commit

---
+++ Bank.java
@@ -0,0 +1,13 @@
+public class Bank {
+    String bankname = "Mark";
+    int bankmoney   = 100;
+    
+    void bankshow1(){
+        System.out.println("----------------------------");
+        System.out.println("[BANK ACCOUNT]");
+        System.out.println("[NAME : "+this.bankname+" ]");
+        System.out.println("[MONEY: "+this.bankmoney+" ]");
+        System.out.println("----------------------------");
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-    
+    Scanner scan = new Scanner(System.in);
+    Bank bank = new Bank();
+
+    bank.bankshow1();
     }
 }
